45da3eb2d96049f5b6d08e8bd08b04f96cc1af47
John Goldade <goldade.john@gmail.com>
2025-05-03 10:44:32 -0500

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -28,7 +28,7 @@ const startApolloServer = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     console.log(__dirname)
-    app.use(express.static(path.join(__dirname, '/client/dist')))
+    app.use(express.static(path.join(__dirname, '../../client/dist')))
 
     
 
